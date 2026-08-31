8a269fd630467bce16ae60144b691ddcea1506be
dbaong <bbbnuu1@gmail.com>
2026-08-31 21:01:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ console.log("JS LOADED");
 const hook=document.querySelector("#hook");
 const start=document.querySelector("#startbutton");
 const gamehall= document.querySelector("#gamehall");
+const conti=document.querySelector("continuebutton");
+const quit=document.querySelector("quit");
+const ifquit = documeny.querySelector("ifquit");
+const close=document.querySelector("closepu");
+const cakescene = document.querySelector("cakescene");
 start.addEventListener("click",() =>{
   hook.style.display="none";
   gamehall.classList.add("open");
+});
+quit.addEventListener("click",() => {
+  ifquit.classList.add("open");
+});
+close.addEventListener("click",() =>{
+  ifquit.classList.remove("open");
+});
+conti.addEventListener("click",() => {
+  gamehall.classList.remove("open");
+  cakescene.classList.add("open");
 });
