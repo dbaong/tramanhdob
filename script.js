89f3c0fc7c97709659e4b3f6e6c3fd51d5a6de01
dbaong <bbbnuu1@gmail.com>
2026-09-03 17:21:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,12 @@ const cakescene = document.querySelector("#cakescene");
 const cake=document.querySelector("#cake");
 const celeb=document.querySelector("#celebrate");
 const falling=document.querySelectorAll(".falling");
+const tinnhan=document.querySelector("#tinnhan");
+const deco=document.querySelector(".deco")
+const con=document.querySelector("#con");
 let clik=0;
+
+
 start.addEventListener("click",() =>{
   hook.style.display="none";
   gamehall.classList.add("open");
@@ -25,6 +30,41 @@ conti.addEventListener("click",() => {
   gamehall.classList.remove("open");
   cakescene.classList.add("open");
 });
+let text=["hilu chăm anhh.",
+          "Hôm nay 30/9 là sinh nhật của Chăm Anhhh.",
+          "Chúc em tuổi 17 sẽ gặp thật nhiều điều vui trong cuộc sống, ngày càng có thêm nhiều kỷ niệm đáng nhớ với bạn bè và người thân.",
+          "Quan trọng hơn, chúc em tuổi mới ngày cảng trổ mã đẹp gái và học giỏi hơn để đậu ULaw và tranh được suất học bổng của trường.",
+          "Cuối cùng, chúc em có 1 ngày sinh nhật vui vẻ và tuổi mới vui tươi với ít nỗi buồn hơn!!!"]
+let start = 0;
+let i = 0;
+let typing = true;
+
+function messa() {
+    if (i < text[start].length) {
+        tinnhan.textContent = text[start].slice(0, i + 1);
+        i++;
+
+        setTimeout(() => {
+            messa();
+        }, 50);
+    } else {
+        typing = false;
+    }
+}
+message.addEventListener("click", () => {
+
+    if (!typing) {
+
+        start++;
+
+        if (start < text.length) {
+            i = 0;
+            typing = true;
+            messa();
+        }
+
+    }
+});
 cake.addEventListener("click",() =>{
 
   clik++;
@@ -36,5 +76,9 @@ cake.addEventListener("click",() =>{
       icon.style.setProperty("--x",`${x}px`)
       icon.style.setProperty("--y",`${y}px`)
     });
+    setTimeout(()=>{
+      message.classList.add("open")
+      messa()
+    },5000)
   }
 });
